aa48eca442f5ae4a13591151a3172d1e95352aee
thsyuna <a.ayunadyaa@gmail.com>
2025-07-04 15:40:41 +0700

    update mail

--- app/controllers/kontaks_controller.ts
+++ app/controllers/kontaks_controller.ts
@@ -1,20 +1,19 @@
-// app/Controllers/Http/KontakController.ts
 import type { HttpContext } from '@adonisjs/core/http'
 import Mail from '@adonisjs/mail/services/main'
 
-
 export default class KontakController {
   async kirimPesan({ request, response }: HttpContext) {
-    const { nama, email, pesan } = request.only(['nama', 'email', 'pesan'])
+    const { nama, email, pesan, source } = request.only(['nama', 'email', 'pesan', 'source'])
 
     try {
       await Mail.send((message) => {
         message
           .from('no-reply@yourdomain.com')
-          .replyTo(email)  
-          .to('dhia.zahrah1511@gmail.com') // ganti dengan email tujuan kamu
-          .subject(`Kritik & Saran dari ${nama}`)
+          .replyTo(email)
+          .to('dhia.zahrah1511@gmail.com')
+          .subject(`[${source}] Kritik & Saran dari ${nama}`)
           .html(`
+            <p><strong>Website:</strong> ${source}</p>
             <p><strong>Nama:</strong> ${nama}</p>
             <p><strong>Email:</strong> ${email}</p>
             <p><strong>Pesan:</strong><br/>${pesan}</p>
